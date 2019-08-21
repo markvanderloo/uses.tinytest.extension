ec5462b615c6036e3696931d492d3c59f4ec18c4
Mark van der Loo <mark.vanderloo@gmail.com>
2019-08-21 08:41:49 +0200

    added test files

---
+++ pkg/inst/tinytest/test_pkg.R
@@ -0,0 +1,13 @@
+
+# simple test
+expect_equal(1 + 1, 2)
+
+library(tinytest.extension)
+
+# should be fine
+expect_lol("lol")
+
+# this should also work
+
+expect_false(ignore(expect_lol)("lulz"))
+
